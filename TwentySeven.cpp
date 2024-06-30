793f0ef72af55a2a5822aa7e82f99a6d568be7fc
Neeraj Kumar <neerajkumar42p@gmail.com>
2024-06-30 11:30:31 +0530

    Commit

---
+++ TwentySeven.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class Student{
+    public: string name;
+    int attendance;
+    float total_marks;
+    void calculate_percentage()
+    {
+        cout<<total_marks<<"%";}};
+        class Client
+        {
+            public:
+            string name;
+            int credit_number;
+            int cvv;
+            string user_name;
+            string password;};
+            int main(){
+                Client a,b,c;
+                a.name="rohit";
+                a.password="Enjoy c++";
+                a.cvv=788;
+                a.credit_number=34723849;
+                cout<<"The password of Rohit is "<<a.password;
+                return 0;
+            }
